0424bdd8fe3fe4d1cecae7d487d9e6b34539a3d9
nazmuldip <nazmuldip21@gmail.com>
2022-09-29 23:15:22 +0600

    how react works describe

--- my-workout/src/components/Question/Question.js
+++ my-workout/src/components/Question/Question.js
@@ -5,6 +5,8 @@ const Question = () => {
     return (
         <div>
             <h1>Hello From Question</h1>
+            <h3>How React works?</h3>
+            <p>Ans: React is a declarative, efficient, and flexible JavaScript library for building user interfaces. 'V' denotes the view in MVC. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application</p>
         </div>
     );
 };
